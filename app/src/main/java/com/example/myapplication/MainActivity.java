34853ad650cae128c2b342f73fb0633739e696e1
marius <marius.lucian.olariu@gmail.com>
2019-05-06 11:02:26 +0100

    Added app drawer UI part

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,41 +1,150 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String MAIN_TAG = "MainActivity";
 
-  private static final String MAIN_TAG = "MainActivity";
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private NavigationView navigationView;
 
-  private SectionsPageAdapter sectionsPageAdapter;
+    private ViewPager viewPager;
 
-  private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_drawer);
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
 
-    sectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
 
-    viewPager = findViewById(R.id.viewPager);
-    setupViewPager(viewPager);
+//        setupDrawer();
 
-    TabLayout tabLay = findViewById(R.id.tabLayout);
-    tabLay.setupWithViewPager(viewPager);
-    setupViewPager(viewPager);
-  }
+        drawerLayout =  findViewById(R.id.nav_drawer);
+        navigationView =  findViewById(R.id.nv);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.Open, R.string.Close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
-  private void setupViewPager(ViewPager viewPager) {
-    SectionsPageAdapter sectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        //set view pager
+        viewPager = findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
 
-    sectionsPageAdapter.addFragment(new ProgressFragment(), "Progress");
-    sectionsPageAdapter.addFragment(new UpcomingFragment(), "Upcoming");
-    sectionsPageAdapter.addFragment(new FeedbackFragment(), "Feedback");
-    sectionsPageAdapter.addFragment(new PastFragment(), "Past");
+        TabLayout tabLay = findViewById(R.id.tabLayout);
+        tabLay.setupWithViewPager(viewPager);
+        setupViewPager(viewPager);
+    }
 
-    viewPager.setAdapter(sectionsPageAdapter );
-  }
+//    @Override
+//    public void onPostCreate(@androidx.annotation.Nullable Bundle savedInstanceState, @androidx.annotation.Nullable PersistableBundle persistentState) {
+//        super.onPostCreate(savedInstanceState, persistentState);
+//    }
+
+    //    private void setupDrawer() {
+//        drawerLayout = (DrawerLayout) findViewById(R.id.nav_drawer);
+//        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
+//
+//        drawerLayout.addDrawerListener(drawerToggle);
+//
+//        drawerToggle.syncState();
+//
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        navigationView = (NavigationView) findViewById(R.id.nv);
+//
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                int itemId = menuItem.getItemId();
+//
+//                switch (itemId) {
+//                    case R.id.sosButton:
+//                        Toast.makeText(MainActivity.this, "Please help", Toast.LENGTH_SHORT).show();
+//                        break;
+//
+//                    case R.id.resetPassB:
+//                        Toast.makeText(MainActivity.this, "Reset Pass", Toast.LENGTH_SHORT).show();
+//                        break;
+//
+//                    case R.id.signOutB:
+//                        Toast.makeText(MainActivity.this, "Sign out", Toast.LENGTH_SHORT).show();
+//                        break;
+//
+//                    default:
+//                        return true;
+//                }
+//
+//                return true;
+//            }
+//
+//        });
+//
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter sectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        sectionsPageAdapter.addFragment(new ProgressFragment(), "Progress");
+        sectionsPageAdapter.addFragment(new UpcomingFragment(), "Upcoming");
+        sectionsPageAdapter.addFragment(new FeedbackFragment(), "Feedback");
+        sectionsPageAdapter.addFragment(new PastFragment(), "Past");
+
+        viewPager.setAdapter(sectionsPageAdapter);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int itemId = menuItem.getItemId();
+
+        switch (itemId) {
+            case R.id.sosButton:
+                Toast.makeText(MainActivity.this, "Please help", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.resetPassB:
+                Toast.makeText(MainActivity.this, "Reset Pass", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.signOutB:
+                Toast.makeText(MainActivity.this, "Sign out", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                return true;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
